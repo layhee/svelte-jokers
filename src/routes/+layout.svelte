8b2e7a28c91ecba3ddf68517342e30f1d08334c3
Lehi Cano <onacihel@gmail.com>
2023-01-28 23:56:38 -0500

    centered main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <style>
     @import "https://unpkg.com/open-props";
     @import "https://unpkg.com/open-props/normalize.min.css";
+    main{
+       display: flex;
+       flex-direction: column;
+       align-items: center;
+       margin: 32px 0;
+    }
 </style>
 <main>
     <slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
   export let data
   import Quotes from "../components/quotes.svelte";
 </script>
-
-<h1>Welcome to Lehi.work</h1>
+<h1>Welcome to Lehi's first Svelte App!</h1>
+<h2>yes, i followed a very helpful tutorial <a href="https://www.youtube.com/watch?v=KHwB2CKM2cE" target="_blank" rel="noreferrer">found here</a></h2>
 <ul>
   {#each data.results as item}
     <li>
